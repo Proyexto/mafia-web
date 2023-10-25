f46650d2a5eb2204d8203aa57492caecbf942cb4
JorgePeniaranda <peniarandajorge@gmail.com>
2023-10-25 10:28:54 -0300

    add react-cookie

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -4,27 +4,34 @@
 import { useRouter } from "next/navigation";
 import { FormEvent } from "react";
 import routes from "@/routes.json";
+import { CookiesProvider, useCookies } from "react-cookie";
 
 /* Styles */
 import style from "./style.module.scss";
 
 export const Home = () => {
   const { push } = useRouter();
+  const [cookies, setCookie] = useCookies(["user"]);
+  setCookie("user", "a", { path: "/" });
+  console.log(cookies);
 
   const onSubmit = (e: FormEvent<HTMLElement>) => {
     e.preventDefault();
 
     // something...
 
+    setCookie("user", "a", { path: "/" });
     push(routes.auth);
   };
   return (
-    <section className={style.homeContainer}>
-      <h1>Mafia Web</h1>
-      <form onSubmit={(e) => onSubmit(e)}>
-        <input type="text" placeholder="Código de Sala" />
-        <button>Ingresar</button>
-      </form>
-    </section>
+    <CookiesProvider>
+      <section className={style.homeContainer}>
+        <h1>Mafia Web</h1>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <input type="text" placeholder="Código de Sala" />
+          <button>Ingresar</button>
+        </form>
+      </section>
+    </CookiesProvider>
   );
 };
