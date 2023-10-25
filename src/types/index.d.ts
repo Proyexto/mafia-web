7ec9a82e005b1ccc2da55c45f0bdfff388914194
trujilloszitai <trujilloszitai@gmail.com>
2023-10-24 22:28:31 -0300

    interfaces

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -5,3 +5,25 @@ export interface Pic {
   height: number;
   author: string;
 }
+
+export interface iButton{
+  text?: string;
+  icon?: HTMLElement;
+  onClick: void;
+  type: "info" | "warning" | "danger" | "neutral" | "primary" | "secondary";
+  className: string;
+}
+
+export interface iInput{
+  placeholder?: string;
+  icon?: HTMLElement;
+  onChange: void;
+  type: "text" | "password" | string;
+  className: string;
+}
+
+export interface iLogo{
+  direction?: "normal" | "reverse";
+  size: "2xl" | "xl" | "lg" | "md" | "sm" | "base";
+
+}
