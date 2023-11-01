8a44d1b54f4253508d769ab2313a749430d17e09
JorgePeniaranda <peniarandajorge@gmail.com>
2023-11-01 12:15:17 -0300

    create connection with mysql database

---
+++ src/app/api/images/route.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { mysql } from "@/libs/mysql";
+
+export async function GET() {
+  try {
+    const responses = await mysql.query("SELECT * FROM `image`");
+
+    return NextResponse.json(responses);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
