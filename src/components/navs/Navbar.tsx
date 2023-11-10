0b321ac4ded4e0f5d6fa74aa1735b2ff9f8909a3
JorgePeniaranda <peniarandajorge@gmail.com>
2023-11-10 13:33:06 -0300

    mejor color

--- src/components/navs/Navbar.tsx
+++ src/components/navs/Navbar.tsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
         <Link href="/" className="flex items-center">
             <Image src="/assets/images/logo.webp" width={512} height={512} className="w-20 mr-3" alt="MafiaWeb-Logo"/>
         </Link>
-        <ul className="flex justify-around gap-12">
+        <ul className="flex justify-around gap-4">
           <li className="flex md:order-2">
             <Link href="/auth/guest" type="button" className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-600 font-medium rounded-lg text-xl px-8 py-2 text-center mr-3 md:mr-0">Jugar</Link>
           </li>
@@ -26,8 +26,8 @@ export const Navbar = () => {
             sessionStatus ? 
               (
                 <li className="flex md:order-2">
-                    <button className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-600 font-medium rounded-lg text-xl px-2 py-2 text-center mr-3 md:mr-0" aria-label="cerrar sesión" onClick={handleClose}>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                    <button className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-600 font-medium rounded-lg text-xl p-2 text-center mr-3 md:mr-0" aria-label="cerrar sesión" onClick={handleClose}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
                         <path fill="currentColor" d="M13 3h-2v10h2V3zm4.83 2.17l-1.42 1.42A6.92 6.92 0 0 1 19 12c0 3.87-3.13 7-7 7A6.995 6.995 0 0 1 7.58 6.58L6.17 5.17A8.932 8.932 0 0 0 3 12a9 9 0 0 0 18 0c0-2.74-1.23-5.18-3.17-6.83z"/>
                       </svg>
                     </button>
